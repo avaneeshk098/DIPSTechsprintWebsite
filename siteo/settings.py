b2f67e920268ec4ca5119daef98ce8dacee67f06
Avaneesh Kumar <avaneeshk098@gmail.com>
2020-10-21 23:29:04 +0530

    Ready to deploy

--- siteo/settings.py
+++ siteo/settings.py
@@ -25,7 +25,7 @@ print(BASE_DIR)
 SECRET_KEY = os.environ.get('PRODUCTION_SECRET', True)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
