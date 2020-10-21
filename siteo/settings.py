1e5ecd9dadfeb7d74fd83d8cd966124092df9482
Avaneesh Kumar <avaneeshk098@gmail.com>
2020-10-21 22:39:35 +0530

    Ready to deploy

--- siteo/settings.py
+++ siteo/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.sites',
     'django.contrib.sitemaps',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
