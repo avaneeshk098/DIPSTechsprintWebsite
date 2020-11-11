f27534f1dbf7ad976ec1575fa9919967db2b6073
Avaneesh Kumar <avaneeshk098@gmail.com>
2020-11-12 01:11:38 +0530

    gspread fixed

--- commerce/views.py
+++ commerce/views.py
@@ -16,7 +16,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 '''
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
-creds = ServiceAccountCredentials.from_json_keyfile_name('\static\cred.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('/static/cred.json', scope)
 
 client = gspread.authorize(creds)
 
